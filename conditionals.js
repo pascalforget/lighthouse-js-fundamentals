67bc79ea69cb6c01aa05a5d150311aa5ce114a81
Pascal Forget <pascal@pascalforget.comgit-it verifygit-it verifylsgit config --global user.email pascal@pascalforget.com>
2020-03-22 14:05:26 +0000

    Exercice de conditionnelles

--- conditionals.js
+++ conditionals.js
@@ -22,18 +22,46 @@
 
 //Troisième version avec plus de conditions Else if
 
-const temperature = 20;
+// const temperature = 20;
 
-if (temperature < 0) {
-    console.log("Make sure you pick out a scarf!");
-  } else if (temperature < 15) {
-    console.log("Short sleeves won't cut it!");
-  } else {
-    console.log("Short sleeves are fine.");
-  }
+// if (temperature < 0) {
+//     console.log("Make sure you pick out a scarf!");
+//   } else if (temperature < 15) {
+//     console.log("Short sleeves won't cut it!");
+//   } else {
+//     console.log("Short sleeves are fine.");
+//   }
   
-  console.log("Now you're ready to go outside!");
+//   console.log("Now you're ready to go outside!");
   
+const age = 10
+
+if (age < 13) {
+    console.log ("Go to Elementary School");
+} else if {age >= 13 && <= 18);
+    console.log("Go to Secondary School")
+} else {
+    console.log("Lighthouse Labs");
+}
+
+
+
+const whichSchool = function (age) {
+    // Your code in here ...
+    if (age < 13); { 
+      return ("Go to Elementary School");
+    } else if (age >= 13 && age <= 18);
+     { return ("Go to Secondary School");
+    } else (age >= 19) {
+      return ("Go to College");
+    }
+}
+    
+
+
+
+
+
 
 
 
