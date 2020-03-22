0070f49a2ec5d5ef6f8cc6376a5d63cde20cb604
Pascal Forget <pascal@pascalforget.comgit-it verifygit-it verifylsgit config --global user.email pascal@pascalforget.com>
2020-03-22 14:11:45 +0000

    Exercice musiciens quartet etc.

---
+++ musicians.js
@@ -0,0 +1,16 @@
+var musicians = 15;
+
+if (musicians === 4) {
+    console.log("solo")
+}
+else if (musicians === 2){
+    console.log("duo");
+} else if (musician === 3){
+    console.log("trio");
+} else if (musician === 4){
+    console.log("quartet");
+} else if (musician <= 0){
+    console.log("this is not a group");
+} else {
+    console.log("this is a large group");
+}
